9e54abeecfd07cd3ed058cfe829bbeeb20646004
Quddus1916 <quddusjunior1916@gmail.com>
2022-06-21 02:08:26 -0500

    visitor registration with image

--- controller/visitor.go
+++ controller/visitor.go
@@ -1,9 +1,13 @@
 package controller
 
 import (
+	"fmt"
 	//"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+	"io"
 	"net/http"
+	"os"
+	"path"
 	// 	"strings"
 	// 	"time"
 	// 	"visitor-management-system/const"
@@ -15,13 +19,42 @@ import (
 
 func CreateVisitor(c echo.Context) error {
 	var visitor = new(model.Visitor)
-	if err := c.Bind(visitor); err != nil {
+
+	visitor.Name = c.FormValue("name")
+	visitor.Address = c.FormValue("address")
+	visitor.CompanyRepresentating = c.FormValue("company_rep")
+	visitor.Email = c.FormValue("email")
+	visitor.Phone = c.FormValue("phone")
+	file, err := c.FormFile("image")
+	if err != nil {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
+	src, err := file.Open()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, file.Header)
+	}
+	defer src.Close()
+
+	uploadedfilename := file.Filename
+	uploadedfilepath := path.Join("./images", uploadedfilename)
+	fmt.Println(uploadedfilepath)
+	dst, err := os.Create(uploadedfilepath)
+	defer dst.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	visitor.ImageName = uploadedfilename
+	visitor.ImagePath = uploadedfilepath
+
 	if err := repository.CreateVisitor(visitor); err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
-	return c.JSON(http.StatusOK, visitor.Id)
+	return c.JSON(http.StatusOK, visitor)
 }
 func GetAllVisitor(c echo.Context) error {
 	// var visitor = new(model.Visitor)
